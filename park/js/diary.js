e7419f4472b30a73b19d4f6ee0a97adc4c914f10
kebi_ko <49586273+kebi3477@users.noreply.github.com>
2021-02-21 21:29:25 +0900

    드래그 방지 테스트 & 크기 변경 추가

--- park/js/diary.js
+++ park/js/diary.js
@@ -4,6 +4,9 @@ const buttonItemRed = document.querySelector(".button__item--red");
 const buttonItemBlue = document.querySelector(".button__item--blue");
 const buttonItemGreeen = document.querySelector(".button__item--green");
 const buttonItemBlack = document.querySelector(".button__item--black");
+const buttonItemLight = document.querySelector(".button__item--light");
+const buttonItemRegular = document.querySelector(".button__item--regular");
+const buttonItemBold = document.querySelector(".button__item--bold");
 // const buttonItemWhite = document.querySelector(".button__item--white");
 const buttonItemReset = document.querySelector(".button__item--reset");
 
@@ -15,8 +18,17 @@ buttonItemGreeen.onclick = () => setStrokeColor("green");
 buttonItemBlack.onclick = () => setStrokeColor("#333");
 // buttonItemWhite.onclick = () => setStrokeColor("#fff");
 buttonItemReset.onclick = () => ctx.clearRect(0, 0, canvas.width, canvas.height);
+buttonItemLight.onclick = () => setStrokeWidth(2);
+buttonItemRegular.onclick = () => setStrokeWidth(6);
+buttonItemBold.onclick = () => setStrokeWidth(10);
+
 
 function setStrokeColor(color) {
     ctx.beginPath();
     ctx.strokeStyle = color;
+}
+
+function setStrokeWidth(width) {
+    ctx.beginPath();
+    ctx.lineWidth = width;
 }
