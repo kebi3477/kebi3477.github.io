1cc471be6e6919d9878466ad323aa749ddedeb0d
kebi_ko <49586273+kebi3477@users.noreply.github.com>
2021-02-26 20:58:12 +0900

    기본 터치 시작 막기

--- park/js/drawing.js
+++ park/js/drawing.js
@@ -30,6 +30,7 @@ function initCanvas(canvas) {
     })();
     // 터치 이벤트로 적용
     canvas.addEventListener("touchstart", function (e) {
+        e.preventDefault();
         mousePos = getTouchPos(canvas, e);
         let touch = e.touches[0];
         let mouseEvent = new MouseEvent("mousedown", {
