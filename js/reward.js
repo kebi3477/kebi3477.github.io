81f99b5e1c63f7200b0b6dfcd9e01525eb6d2397
kebi_ko <49586273+kebi3477@users.noreply.github.com>
2021-03-08 20:53:17 +0900

    다이어리 이벤트 등 추가

---
+++ js/reward.js
@@ -0,0 +1,9 @@
+const write = document.querySelector(".write");
+const conti = document.querySelector(".continue");
+
+write.addEventListener("click", function() {
+    location.href = 'diary.html';
+})
+conti.addEventListener("click", function() {
+    location.href = 'main.html';
+})
