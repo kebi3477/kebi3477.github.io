7fda6c016aa6ae19e740b8c06e0fb6c0a692d11f
kebi3477 <kebi3477@naver.com>
2021-04-11 17:46:11 +0900

    개별 시간 위치 및 값 수정

--- js/reward.js
+++ js/reward.js
@@ -3,13 +3,14 @@ const conti = document.querySelector(".continue");
 const timeoutDom = document.querySelector(".timeout");
 const timeout = JSON.parse(localStorage.getItem("timeout"));
 const timeoutItems = document.querySelectorAll('.timeout-item');
-const time = timeout.game1 + timeout.game2 + timeout.game3 + timeout.game4 + timeout.game5;
-const minute = Math.floor(time/60);
-const seconds = Math.floor(time%60);
+let time = 0, minute = 0, seconds = 0;
 
 Object.values(timeout).forEach((value, index) => {
     timeoutItems[index].innerText = `${Math.floor(value)}초`;
+    time += Math.floor(value);
 })
+minute = Math.floor(time/60);
+seconds = Math.floor(time%60);
 
 timeoutDom.innerText = `${minute}분 ${seconds}초`;
 write.addEventListener("click", function() {
